3377fce73afaf230f8fe393b5a0dc537f59d117c
rin-yato <chearithorn@gmail.com>
2022-10-13 11:18:49 +0700

    bug: add items

---
+++ components/Aside.js
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { useContext } from "react";
+import { Context } from "../layouts/Layout";
+
+export default function Aside() {
+  const { isOpen, setValue } = useContext(Context);
+
+  const toggleDrawer =
+    (open, delay = 0) =>
+    (event) => {
+      if (
+        event.type === "keydown" &&
+        (event.key === "Tab" || event.key === "Shift")
+      ) {
+        return;
+      }
+
+      setTimeout(() => {
+        setValue(open);
+      }, delay);
+    };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onKeyDown={toggleDrawer(false)}
+    >
+      <Button className="float-right z-30 p-1 mr-1.5 mt-1 min-w-min min-h-min rounded-full" onClick={toggleDrawer(false, 250)}>
+        <i className="bx bx-x bx-sm text-black"></i>
+      </Button>
+      <List className="pt-20">
+        {["Sneakers", "Accessories", "About", "Contact"].map((text, index) => (
+          <ListItem button key={text} className="my-4">
+            <ListItemText
+              primary={text}
+              disableTypography
+              className=" text-center font-semibold"
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <React.Fragment key={"right"}>
+        <Drawer anchor={"right"} open={isOpen} onClose={toggleDrawer(false)}>
+          {list()}
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
+}
---
+++ components/Nav.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Button from "@mui/material/Button";
+import { useContext } from "react";
+import { Context } from "../layouts/Layout";
+
+function Nav() {
+
+  const { setValue, isOpen } = useContext(Context);
+  
+  const handleToggle = () => {
+    setValue(!isOpen);
+    console.log(isOpen);
+  };
+
+  return (
+    <nav
+      className="
+    bg-white px-3 pr-2.5 p-1 w-screen flex justify-between items-center
+      fixed top-0 left-0 "
+    >
+      <Image
+        src="/images/logo.jpeg"
+        className=" rounded-full ml-1"
+        width={40}
+        height={40}
+        alt=""
+      />
+      <div className="h-min">Daily Kicks</div>
+      <Button className="p-0 pl-1 min-w-min min-h-min ml-1 rounded-full" onClick={handleToggle}>
+        <i className="bx bx-menu text-2xl text-slate-900"></i>
+      </Button>
+    </nav>
+  );
+}
+
+export default Nav;
